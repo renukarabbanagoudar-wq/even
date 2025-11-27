dfe9b403432f8080414bc590e6f92751bc1fc730
Renuka <renuka.rabbanagoudar@gmail.com>
2025-11-27 16:17:26 +0530

    Update even.py

--- even.py
+++ even.py
@@ -1,18 +1,15 @@
 import sys
-if len(sys.argv) <2:
-  print("Usage: python "even_odd_count.py" num1 num2 num3....")
-    sys.exist(1)
+if len(sys.argv)==2:
+ script_name=(sys.argv)[0]
 numbers = sys.argv[1:]
 even_count=0
 odd_count=0
 for n in numbers:
-  try:
+else:
     num=int(n)
     if num % 2 ==0
        even_count +=1
     else:
-       odd_count +=1
-expect ValueError:
-     print("Invalid number is skipped{n}")
+       odd_count +=1     
 print("Count of Even numbers:",even_count)
 print("Count of Odd numbers:",odd_count)
